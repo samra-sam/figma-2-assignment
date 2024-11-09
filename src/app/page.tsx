fc86fb387c20f854046bb7ea8232210edcd83e5b
samra <samraabdulhameed7@gmail.com>
2024-11-09 19:07:40 +0500

    figma-3-assigment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,283 @@
 import Image from "next/image";
+import logo from "../app/assets/Logo.png";
+import buttonLogo from "@/app/assets/Group 212.png";
+import workTogether from "@/app/assets/Work Together Image.png";
+import sponsers from "@/app/assets/sponsors.png";
+import footerLogo from "@/app/assets/©2021 Whitepace LLC..png";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function Page() {
+    return (
+        <div>
+            <nav className="flex justify-between items-center bg-[#043873] px-[220px] py-4 h-[92px]">
+                <div className="flex items-center gap-2">
+                    {/* Use Next.js Image component for optimized loading and compatibility */}
+                    <Image src={logo} alt="Logo" width={150} height={80} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                </div>
+                <ul className="flex gap-8 list-none m-0">
+                    <li className="text-white font-dm-sans font-medium text-lg leading-6 cursor-pointer">
+                        Products
+                    </li>
+                    <li className="text-white font-dm-sans font-medium text-lg leading-6 cursor-pointer">
+                        Solutions
+                    </li>
+                    <li className="text-white font-dm-sans font-medium text-lg leading-6 cursor-pointer">
+                        Resources
+                    </li>
+                    <li className="text-white font-dm-sans font-medium text-lg leading-6 cursor-pointer">
+                        Pricing
+                    </li>
+                </ul>
+                <button className="bg-[#FFE492] text-[#043873] py-4 px-10 font-inter font-medium text-lg leading-6 rounded-[8px] cursor-pointer hover:bg-[#E6DB8A]">
+                    Login
+                </button>
+            </nav>
+            <div className="flex justify-between bg-[#043873] py-[140px] px-[220px] gap-[50px]">
+                <div className="w-[1080px] h-[520px] -ml-24 flex gap-[40px]">
+
+                    {/* Text Section */}
+                    <div className="flex flex-col justify-center gap-[60px] text-white max-w-[656px]">
+                        {/* Text Block */}
+                        <div className="flex flex-col gap-[24px]">
+                            <h2 className="font-bold text-[64px] leading-[77.45px] tracking-[-0.02em] mb-6">
+                                Get More Done with  Whitepace
+                            </h2>
+                            <p className="text-[18px] font-normal leading-[30px] tracking-[-0.36px]">
+                                Project management software that enables your teams to collaborate, plan, analyze, and manage everyday tasks
+                            </p>
+                        </div>
+
+
+                        {/* Button */}
+                        <div className="w-fit h-[45px] bg-[#4F9CF9] text-white rounded-lg gap-[10px] py-3 pl-6 flex items-center justify-between text-sm mt-8">
+                            <button className="flex items-center gap-4">
+                                Try Whitepace Free
+                                <Image src={buttonLogo} alt="buttonLogo" className="w-auto h-4 pr-2" />
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Empty Container Aligned to Right */}
+                    <div className="w-[800px] h-[450px] bg-[#C4DEFD] ml-auto"></div>
+                </div>
+            </div>
+            <div className=" bg-white h-[1588px] w-full py-[80px] px-[130px] ">
+                <div className=" w-[1080px] h-[500px] mt-[160px]  gap-[40px] flex ">
+                    <div className="w-[450px] h-[360px] mt-16 ">
+                        <div className="w-[450px] h-[230px] gap-6">
+                            <h1 className="font-semibold text-5xl leading-[70.14px] tracking-[-2%] text-[#212529] ">
+                                Project Management
+                            </h1>
+                            <p className="font-normal text-xs leading-6 text-[#212529] mt-4">
+                                Images, videos, PDFs and audio files are supported. Create math expressions and diagrams
+                                directly from the app. Take photos with the mobile app and save them to a note.
+                            </p>
+
+                            {/* button */}
+
+                            <div className="w-[140px] h-[45px] bg-[#4F9CF9] text-white rounded-lg gap-[10px] py-3 pl-2
+                    font-normal tracking-[-2%] flex text-sm mt-20">
+                                <button className="flex justify-between  gap-4">Get Started
+                                    <Image src={buttonLogo} alt="buttonLogo" className="w-auto mt-1 " />
+                                </button>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+
+                    {/* empty container */}
+                    <div className="w-[600px] h-[500px] bg-[#C4DEFD] "></div>
+                </div>
+
+                {/* work together page */}
+
+                <div className=" w-[1080px] h-[500px] mt-[160px] flex gap-[150px]">
+                    {/* image div */}
+                    <div>
+                        <Image src={workTogether} alt="workTogetherImage" className=" w-[480px] mt-7" />
+                    </div>
+
+                    {/* work together div */}
+                    <div className="w-[455px] h-[230px]  mt-32 items-end">
+                        <div className="w-[455px] h-[171px] ">
+                            <h1 className="font-semibold text-5xl leading-[70.14px] tracking-[-2%] text-[#212529]">
+                                Work together
+                            </h1>
+                            <p className="font-normal text-xs leading-6 text-[#212529] mt-4">
+                                With whitepace, share your notes with your colleagues and collaborate on them.
+                                You can also publish a note to the internet and share the URL with others.
+                            </p>
+
+
+                            {/* button */}
+
+                            <div className="w-[120px] flex justify-center h-[45px] bg-[#4F9CF9] text-white rounded-lg gap-[10px] py-3 pl-2
+                        font-normal tracking-[-2%] text-sm mt-12">
+                                <button className="flex justify-between gap-4">Try it now
+                                    <Image src={buttonLogo} alt="buttonLogo" className="w-auto mt-1 " />
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            {/* use an Extension page */}
+
+            <div className=" h-[800px] w-full py-[8px] px-[130px] bg-[#043873] ">
+                <div className=" w-[1080px] h-[500px] mt-[160px] gap-[40px] flex ">
+
+                    <div className="w-[450px] h-[280px]  mt-28 ">
+                        <div className="w-[450px] h-[180px] gap-6">
+                            <h1 className="font-semibold text-5xl leading-[70.14px] tracking-[-2%] text-white ">Use as Extension</h1>
+                            <p className="font-normal text-xs leading-6 text-white mt-4">
+                                Use the web clipper extension, available on Chrome and Firefox, to save web pages or take screenshots as notes.
+                            </p>
+
+
+                            {/* button */}
+
+                            <div className="w-[140px] h-[45px] bg-[#4F9CF9] text-white rounded-lg gap-[10px] py-3 pl-6
+                        font-normal tracking-[-2%] text-sm mt-24">
+                                <button className="flex justify-between gap-4">Let's Go
+                                    <Image src={buttonLogo} alt="buttonLogo" className="w-auto mt-1 " />
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    {/* empty container */}
+                    <div className="w-[600px] h-[500px] bg-[#C4DEFD] "></div>
+
+                </div>
+            </div>
+
+            {/* costumize it to your needs page */}
+            <div className=" h-[850px] w-full py-[8px] px-[130px] bg-white ">
+                <div className=" w-[1080px] h-[500px] mt-[160px] gap-[40px] flex ">
+
+
+                    {/* empty container */}
+                    <div className="w-[650px] h-[500px] bg-[#C4DEFD] "></div>
+
+                    <div className="w-[440px] h-[325px]  mt-24  ">
+                        <div className="w-[440px] h-[230px] gap-6">
+                            <h1 className="font-semibold text-5xl leading-[70.14px] tracking-[-2%] text-[#212529] ">
+                                Customise it to your needs
+                            </h1>
+                            <p className="font-normal text-xs leading-6 text-[#212529] mt-4">
+                                Customise the app with plugins, custom themes and multiple text editors
+                                (Rich Text or Markdown). Or create your own scripts and plugins using the Extension API.
+                            </p>
+
+                            {/* button */}
+
+                            <div className="w-[140px] h-[45px] bg-[#4F9CF9] text-white rounded-lg gap-[10px] py-3 pl-6
+                            font-normal tracking-[-2%] text-sm mt-12">
+                                <button className="flex justify-between gap-4">Let's Go
+                                    <Image src={buttonLogo} alt="buttonLogo" className="w-auto mt-1 " />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/*your work every whre you are */}
+            <div className=" h-[400px] w-full py-[85px] px-[150px] bg-[#043873] ">
+                <div className=" w-[1080px] h-[250px] ">
+                    <div className="w-[738px] h-[150px] ml-[180px] ">
+                        <h1 className="font-semibold text-5xl leading-[70.14px] tracking-[-2%] text-white  ">Your work, everywhere you are</h1>
+                        <p className="font-normal text-xs leading-6 text-white mt-4 text-center">
+                            Access your notes from your computer, phone or tablet by synchronising with various services, including whitepace,
+                            Dropbox and OneDrive. The app is available on Windows, macOS, Linux, Android and iOS. A terminal app is also available!
+                        </p>
+
+
+
+                        <div className="w-[140px] h-[45px] bg-[#4F9CF9] text-white rounded-lg gap-[10px] py-3 pl-6
+                            font-normal tracking-[-2%] text-sm mt-16 ml-[280px]">
+                            <button className="flex justify-between gap-4">Try Taskey
+                                <Image src={buttonLogo} alt="buttonLogo" className="w-auto mt-1 " />
+                            </button>
+                        </div>
+                    </div>
+                </div >
+            </div >
+
+
+            {/* our sponsers page */}
+            <div className=" h-[386px] w-full py-[85px] px-[150px] bg-white ">
+                <div className=" w-[1080px] h-fit  text-[#212529] font-semibold text-5xl leading-[70.14px] tracking-[-2%] text-center">Our sponsors
+                    <div className=" w-[1080px] h-fit mt-[84px]">
+                        <Image src={sponsers} alt="sponsers logo" />
+                    </div>
+                </div>
+            </div>
+            <div className=" h-[400px] w-full py-[120px] px-[130px] bg-[#043873] ">
+                <div className="w-[1080px] h-[180px]   gap-[20px] flex">
+                    {/* div1 */}
+                    <div className="w-[258px]  text-white">
+                        <Image src={logo} alt="logo" />
+                        <div className="w-[205px] mt-5" >
+                            <p>whitepace was created for the new ways we live and work. We make a better workspace around the world</p>
+                        </div>
+                    </div>
+
+                    {/* div2 */}
+
+                    <div className="w-[258px] text-white">
+                        <h1 className="font-bold text-base  leading-[21.78px]">Product</h1>
+                        <ul >
+                            <li className="mt-3">Overveiw </li>
+                            <li className="mt-3">Pricig</li>
+                            <li className="mt-3">Costumer Stories</li>
+                        </ul>
+                    </div>
+
+
+
+
+                    {/* div3 */}
+                    <div className="w-[258px]  text-white">
+                        <h1 className="font-bold text-base  leading-[21.78px]">Resources</h1>
+                        <ul >
+                            <li className="mt-3">Blog</li>
+                            <li className="mt-3">Guides & Totorials</li>
+                            <li className="mt-3">Help Center</li>
+                        </ul>
+                    </div>
+
+
+
+
+                    {/* div4 */}
+
+
+                    <div className="w-[258px]  text-white">
+                        <h1 className="font-bold text-base  leading-[21.78px]">Company</h1>
+                        <ul >
+                            <li className="mt-3">About us</li>
+                            <li className="mt-3">Careers</li>
+                            <li className="mt-3">Media Kit</li>
+                        </ul>
+                    </div>
+
+
+
+
+
+                </div>
+
+                <Image src={footerLogo} alt="footerLogo" className="mt-20 ml-[450px]" />
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+    );
 }
